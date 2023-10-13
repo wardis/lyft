0e1db00c23deea3e2b937729e6cdf653c9aaaac5
Wardi Soulaimana <wardi@wardi-s.com>
2023-10-13 22:26:22 +0100

    manage exercise state

---
+++ app/log-workout/components/Exercise.tsx
@@ -0,0 +1,227 @@
+"use client";
+
+import React, { useState, useId } from "react";
+
+import { Button } from "@/components/ui/Button";
+import { Checkbox } from "@nextui-org/checkbox";
+import { Avatar } from "@nextui-org/avatar";
+import {
+  Table,
+  TableHeader,
+  TableBody,
+  TableColumn,
+  TableRow,
+  TableCell,
+} from "@nextui-org/table";
+import { BiDotsVertical, BiDumbbell, BiPlus } from "react-icons/bi";
+import { MdDone } from "react-icons/md";
+import { CgTimer } from "react-icons/cg";
+import { Input } from "@nextui-org/input";
+
+type Set = {
+  position: number;
+  weight?: number;
+  reps?: number;
+  isDone: boolean;
+};
+
+type Props = {
+  exercise: any;
+};
+
+const columns = [
+  { name: "SET", uid: "set" },
+  { name: "PREVIOUS", uid: "previous" },
+  {
+    name: (
+      <div className="flex items-center gap-1">
+        <BiDumbbell />
+        KG
+      </div>
+    ),
+    uid: "weight",
+  },
+  { name: "REPS", uid: "reps" },
+  { name: <MdDone />, uid: "done" },
+];
+
+export default function Exercise({ exercise }: Props) {
+  const [sets, setSets] = useState<Set[]>([
+    {
+      position: 1,
+      isDone: false,
+    },
+  ]);
+
+  const addSet = () => {
+    setSets([
+      ...sets,
+      {
+        position: sets.length + 1,
+        isDone: false,
+      },
+    ]);
+  };
+
+  const deleteSet = (position: number) => {
+    setSets((oldSets) =>
+      oldSets
+        .filter((set) => set.position !== position)
+        .map((set, index) => ({ ...set, position: index + 1 }))
+    );
+  };
+
+  const changeWeight = (position, value) => {
+    setSets((oldSets) =>
+      oldSets.map((set) => {
+        if (set.position === position) {
+          return { ...set, weight: value };
+        }
+        return set;
+      })
+    );
+  };
+  const changeReps = (position, value) => {
+    setSets((oldSets) =>
+      oldSets.map((set) => {
+        if (set.position === position) {
+          return { ...set, reps: value };
+        }
+        return set;
+      })
+    );
+  };
+  const changeDone = (position, value) => {
+    setSets((oldSets) =>
+      oldSets.map((set) => {
+        if (set.position === position) {
+          return { ...set, isDone: value };
+        }
+        return set;
+      })
+    );
+  };
+
+  const renderCell = (set: Set, columnKey: React.Key) => {
+    const cellValue = set[columnKey as keyof Set];
+
+    switch (columnKey) {
+      case "set":
+        return (
+          <Button
+            isIconOnly
+            size="sm"
+            variant="light"
+            className=""
+            onClick={() => {
+              deleteSet(set.position);
+            }}
+          >
+            {set.position}
+          </Button>
+        );
+      case "previous":
+        return "--";
+      case "weight":
+        return (
+          <Input
+            size="sm"
+            variant="flat"
+            classNames={{
+              inputWrapper: "bg-transparent shadow-none",
+            }}
+            placeholder={set.previousWeight ?? "0"}
+            value={set.weight}
+            onChange={(e) => changeWeight(set.position, Number(e.target.value))}
+          />
+        );
+      case "reps":
+        return (
+          <Input
+            size="sm"
+            classNames={{
+              inputWrapper: "bg-transparent shadow-none",
+            }}
+            type="number"
+            placeholder={set.previousReps ?? "0"}
+            value={set.reps}
+            onChange={(e) => changeReps(set.position, Number(e.target.value))}
+          />
+        );
+      case "done":
+        return (
+          <Checkbox
+            checked={set.isDone}
+            onChange={(e) => changeDone(set.position, e.target.checked)}
+          />
+        );
+      default:
+        return cellValue;
+    }
+  };
+
+  return (
+    <div className="flex-col gap-2 flex">
+      <div className="flex items-center gap-2 justify-start">
+        <Avatar
+          size="sm"
+          src="/assets/dumbbell.svg"
+          style={{ padding: "4px" }}
+        />
+        <p className="text-primary flex-grow">{exercise.name}</p>
+        <BiDotsVertical className="text-primary" size="24" />
+      </div>
+
+      <Input
+        size="sm"
+        variant="flat"
+        classNames={{
+          inputWrapper: "bg-transparent shadow-none w-full text-sm",
+        }}
+        placeholder="Add notes here..."
+      />
+
+      <div className="text-primary flex items-center gap-2">
+        <Button color="primary" variant="light">
+          <CgTimer size={24} />
+          Rest Timer: 35s
+        </Button>
+      </div>
+
+      <Table
+        removeWrapper
+        aria-label="sets"
+        isCompact
+        classNames={{
+          base: "text-center",
+          th: "bg-transparent text-center",
+        }}
+      >
+        <TableHeader columns={columns}>
+          {(column) => (
+            <TableColumn key={column.uid}>{column.name}</TableColumn>
+          )}
+        </TableHeader>
+        <TableBody items={sets}>
+          {(set) => (
+            <TableRow key={set.position}>
+              {(columnKey) => (
+                <TableCell>{renderCell(set, columnKey)}</TableCell>
+              )}
+            </TableRow>
+          )}
+        </TableBody>
+      </Table>
+
+      <Button
+        size="sm"
+        className=" w-2/4 mx-auto"
+        onClick={() => {
+          addSet();
+        }}
+      >
+        <BiPlus /> Add Set
+      </Button>
+    </div>
+  );
+}
--- app/log-workout/page.tsx
+++ app/log-workout/page.tsx
@@ -2,33 +2,15 @@
 
 import { Button } from "@/components/ui/Button";
 import { Divider } from "@nextui-org/divider";
-import { Avatar } from "@nextui-org/avatar";
-import { Checkbox } from "@nextui-org/checkbox";
-import {
-  Table,
-  TableHeader,
-  TableBody,
-  TableColumn,
-  TableRow,
-  TableCell,
-} from "@nextui-org/table";
-import { useState } from "react";
-import { BiDotsVertical, BiDumbbell, BiPlus } from "react-icons/bi";
-import { MdDone } from "react-icons/md";
-import { CgTimer } from "react-icons/cg";
+import { BiDumbbell, BiPlus } from "react-icons/bi";
+
 import { Link } from "@nextui-org/link";
-import { Input } from "@nextui-org/input";
+import { useState } from "react";
+import Exercise from "./components/Exercise";
 
 type Exercise = {
   id: number;
   name: string;
-};
-
-type Set = {
-  weight: number;
-  reps: number;
-  isDone: boolean;
-  exerciseIndex: number;
 };
 
 const mockExercises = [
@@ -37,8 +19,6 @@ const mockExercises = [
   { id: 3, name: "Front Squat" },
   { id: 4, name: "Romanian Deadlift" },
 ];
-
-const defaultSet = { weight: 0, reps: 0, done: false };
 
 const formatPreviousSet = ({
   weight,
@@ -49,34 +29,13 @@ const formatPreviousSet = ({
 }) => `${weight}kg x ${reps}`;
 
 export default function LogWorkout() {
-  const [exercises, setExercises] = useState<Exercise[]>([]);
-  const [sets, setSets] = useState<Set[]>([]);
+  const [exercises, setExercises] = useState<Exercise[]>([mockExercises[0]]);
 
   const addExercise = () => {
     setExercises([
       ...exercises,
       {
         ...mockExercises[Math.floor(Math.random() * mockExercises.length)],
-      },
-    ]);
-
-    const newExerciseIndex = exercises.length;
-    console.log(exercises, newExerciseIndex);
-    if (
-      sets.filter((set) => set.exerciseIndex === newExerciseIndex).length === 0
-    ) {
-      addSet(newExerciseIndex);
-    }
-  };
-
-  const addSet = (exerciseIndex: number) => {
-    setSets([
-      ...sets,
-      {
-        weight: 0,
-        reps: 0,
-        isDone: false,
-        exerciseIndex,
       },
     ]);
   };
@@ -123,122 +82,7 @@ export default function LogWorkout() {
       ) : (
         <div className="py-4 flex flex-col gap-8">
           {exercises.map((exercise, index) => (
-            <div
-              key={`${index}-${exercise.name}`}
-              className="flex-col gap-2 flex"
-            >
-              <div className="flex items-center gap-2 justify-start">
-                <Avatar
-                  size="sm"
-                  src="/assets/dumbbell.svg"
-                  style={{ padding: "4px" }}
-                />
-                <p className="text-primary flex-grow">
-                  {exercise.name} {index}
-                </p>
-                <BiDotsVertical className="text-primary" size="24" />
-              </div>
-
-              <Input
-                size="sm"
-                variant="flat"
-                classNames={{
-                  inputWrapper: "bg-transparent shadow-none w-full text-sm",
-                }}
-                placeholder="Add notes here..."
-              />
-
-              <div className="text-primary flex items-center gap-2">
-                <Button color="primary" variant="light">
-                  <CgTimer size={24} />
-                  Rest Timer: 35s
-                </Button>
-              </div>
-
-              <Table
-                removeWrapper
-                aria-label="sets"
-                isCompact
-                classNames={{
-                  base: "text-center",
-                  th: "bg-transparent text-center",
-                }}
-              >
-                <TableHeader>
-                  <TableColumn className="uppercase">Set</TableColumn>
-                  <TableColumn className="uppercase">Previous</TableColumn>
-                  <TableColumn className="uppercase">
-                    <div className="flex items-center gap-1">
-                      <BiDumbbell />
-                      kg
-                    </div>
-                  </TableColumn>
-                  <TableColumn className="uppercase">Reps</TableColumn>
-                  <TableColumn className="uppercase">
-                    <MdDone />
-                  </TableColumn>
-                </TableHeader>
-                <TableBody>
-                  {sets
-                    .filter((set) => set.exerciseIndex === index)
-                    .map((set, index) => (
-                      <TableRow key={index + 1}>
-                        <TableCell>
-                          <Button
-                            isIconOnly
-                            size="sm"
-                            variant="light"
-                            className=""
-                            onClick={() => {
-                              alert("Remove set ?");
-                            }}
-                          >
-                            {index + 1}
-                          </Button>
-                        </TableCell>
-                        <TableCell className="text-default-300 w-2/6">
-                          {exercise.previousSet
-                            ? formatPreviousSet(exercise.previousSet)
-                            : "--"}
-                        </TableCell>
-                        <TableCell>
-                          <Input
-                            size="sm"
-                            variant="flat"
-                            classNames={{
-                              inputWrapper: "bg-transparent shadow-none",
-                            }}
-                            placeholder={exercise.previousSet?.weight || 0}
-                          />
-                        </TableCell>
-                        <TableCell>
-                          <Input
-                            size="sm"
-                            classNames={{
-                              inputWrapper: "bg-transparent shadow-none",
-                            }}
-                            placeholder={exercise.previousSet?.reps || 0}
-                          />
-                        </TableCell>
-                        <TableCell>
-                          <Checkbox />
-                        </TableCell>
-                      </TableRow>
-                    ))}
-                </TableBody>
-              </Table>
-
-              <Button
-                size="sm"
-                className=" w-2/4 mx-auto"
-                onClick={() => {
-                  console.log(index);
-                  addSet(index);
-                }}
-              >
-                <BiPlus /> Add Set
-              </Button>
-            </div>
+            <Exercise key={`${index}-${exercise.name}`} exercise={exercise} />
           ))}
         </div>
       )}
