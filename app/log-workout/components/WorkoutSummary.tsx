cf407640ba494d98999a8b51fe9a4a5c9544315e
Wardi Soulaimana <wardi@wardi-s.com>
2023-10-16 23:09:59 +0100

    add progress bar

--- app/log-workout/components/WorkoutSummary.tsx
+++ app/log-workout/components/WorkoutSummary.tsx
@@ -3,6 +3,7 @@
 import React from "react";
 
 import { Divider } from "@nextui-org/divider";
+import { Progress } from "@nextui-org/progress";
 import { useFormContext } from "react-hook-form";
 
 import WorkoutDuration from "./WorkoutDuration";
@@ -28,6 +29,11 @@ export default function WorkoutSummary({}: Props) {
   return (
     <div>
       <div className="bg-background ">
+        <Progress
+          size="sm"
+          aria-label="workout-progress"
+          value={(completedSetCount / totalSetCount) * 100}
+        />
         <div className="grid grid-cols-3 py-2">
           <div>
             <p>Duration</p>
