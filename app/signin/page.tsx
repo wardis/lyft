420f62eec43077b9357a32014f099889c1a6975d
Wardi Soulaimana <wardi@wardi-s.com>
2023-10-22 14:08:10 +0100

    remove vercel logo signin

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -15,16 +15,6 @@ export default async function SignIn() {
   return (
     <div className="flex justify-center height-screen-helper">
       <div className="flex flex-col justify-between max-w-lg p-3 m-auto w-80 ">
-        <div className="flex justify-center pb-12 ">
-          <Image
-            src="/vercel.svg"
-            alt="Vercel Logo"
-            className="dark:invert"
-            width={100}
-            height={24}
-            priority
-          />
-        </div>
         <AuthUI />
       </div>
     </div>
